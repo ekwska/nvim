c8c95a0f68fb9a78e013b4ea14c42373eb0d15f0
ekwska <ekwiecinska55@gmail.com>
2026-07-12 22:46:03 +0100

    feat: ✨ add alpha-nvim and startup screen

--- init.lua
+++ init.lua
@@ -972,6 +972,8 @@ do
   require 'kickstart.plugins.neo-tree'
   require 'kickstart.plugins.autopairs'
   require 'kickstart.plugins.lazygit'
+  require 'kickstart.plugins.alpha'
+
   -- require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
 
   -- NOTE: You can add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`

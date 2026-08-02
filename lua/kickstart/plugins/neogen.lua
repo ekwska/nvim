f44fdaa70c222b75513ba540a7a6eb3ac4faa0ed
ekwska <ekwiecinska55@gmail.com>
2026-08-02 21:59:16 +0100

    add: ➕ neogen keymappings
    add: ➕ neogen keymappings

--- lua/kickstart/plugins/neogen.lua
+++ lua/kickstart/plugins/neogen.lua
@@ -1,4 +1,11 @@
 vim.pack.add({
     'https://github.com/danymat/neogen',
 })
-require("neogen").setup()
+require("neogen").setup {
+    config = true
+}
+
+vim.keymap.set("n", "<leader>ng", ":lua require('neogen').generate()<CR>", { desc = "Neogen: Generate", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>nc", ":lua require('neogen').generate({ type = 'class' })<CR>", { desc = "Neogen: Class", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>nf", ":lua require('neogen').generate({ type = 'function' })<CR>", { desc = "Neogen: Function", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>nt", ":lua require('neogen').generate({ type = 'type' })<CR>", { desc = "Neogen: Type", noremap = true, silent = true })
